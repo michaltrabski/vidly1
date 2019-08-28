2aeade7112178ff779fe6c3bdd92bd1a358c1b26
Michal Trabski <michal.trabski@gmail.com>
2019-08-28 14:29:43 +0200

    added movies pagination mechanism

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
-    movies: getMovies()
+    movies: getMovies(),
+    currentPage: 1,
+    pageSize: 4
   };
 
   handleDelete = movie => {
@@ -11,10 +15,17 @@ class Movies extends Component {
     this.setState({ movies });
   };
 
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
     const { length: count } = this.state.movies;
+    const { movies, currentPage, pageSize } = this.state;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const paginatedMovies = paginate(movies, currentPage, pageSize);
 
     return (
       <React.Fragment>
@@ -30,7 +41,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {paginatedMovies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
@@ -48,6 +59,12 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
---
+++ src/components/pagination.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+
+const Pagination = props => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+
+  const pages = _.range(1, pagesCount + 1);
+
+  // console.log(pagesCount);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(x => (
+          <li
+            key={x}
+            className={x === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link " href="#" onClick={() => onPageChange(x)}>
+              {x}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+Pagination.propTypes = {
+  itemsCount: PropTypes.number.isRequired,
+  pageSize: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired
+};
+
+export default Pagination;
