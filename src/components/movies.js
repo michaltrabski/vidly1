deb42e142e751d85772ae427f6141348d5a4c9a3
Michal Trabski <michal.trabski@gmail.com>
2019-08-30 14:04:17 +0200

    created MovieTable component and moved table from movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -4,7 +4,7 @@ import Pagination from "components/pagination";
 import { paginate } from "utils/paginate";
 import ListGroup from "components/listGroup";
 import { getGenres } from "services/fakeGenreService";
-// import { listenerCount } from "cluster";
+import MovieTable from "components/moviesTable";
 
 class Movies extends Component {
   state = {
@@ -12,7 +12,7 @@ class Movies extends Component {
     genres: [],
     selectedGenres: "",
     currentPage: 1,
-    pageSize: 4
+    pageSize: 2
   };
 
   componentDidMount() {
@@ -64,35 +64,7 @@ class Movies extends Component {
           <p className="text-center m-3">
             Showing {filteredMovies.length} movies in the database.
           </p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>Rate</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {paginatedMovies.map(movie => (
-                <tr key={movie._id}>
-                  <td>{movie.title}</td>
-                  <td>{movie.genre.name}</td>
-                  <td>{movie.numberInStock}</td>
-                  <td>{movie.dailyRentalRate}</td>
-                  <td>
-                    <button
-                      onClick={() => this.handleDelete(movie)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <MovieTable movies={paginatedMovies} onDelete={this.handleDelete} />
           <Pagination
             itemsCount={filteredMovies.length}
             pageSize={pageSize}
